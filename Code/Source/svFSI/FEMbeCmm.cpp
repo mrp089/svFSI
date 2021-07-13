2ef9ab90d5e346258d2eb6c547d4621bef9ee33d
mrp089 <51269309+mrp089@users.noreply.github.com>
2021-07-13 11:38:50 -0700

    remove passing xgp to matmodels

--- Code/Source/svFSI/FEMbeCmm.cpp
+++ Code/Source/svFSI/FEMbeCmm.cpp
@@ -22,7 +22,7 @@
 extern"C"
 {
 
-void stress_tangent_(const double* Fe, const double* fl, const double* xgp, const double* time, double* S_out, double* CC_out)
+void stress_tangent_(const double* Fe, const double* fl, const double* time, double* S_out, double* CC_out)
 {
 	// convert deformation gradient to FEBio format
 	mat3d F(Fe[0], Fe[3], Fe[6], Fe[1], Fe[4], Fe[7], Fe[2], Fe[5], Fe[8]);
